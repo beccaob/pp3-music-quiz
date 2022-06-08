5365e030c5f41d46fa5597be62291aab8e53a11a
beccaob <rob3651@outlook.com>
2022-06-08 20:11:42 +0000

    remove credentials

--- run.py
+++ run.py
@@ -1,18 +1,4 @@
-import gspread
-from google.oauth2.service_account import Credentials
 import quiz
-
-
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
-
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('music_quiz')
 
 # title message
 print("Welcome to my Music Quiz!\n")
