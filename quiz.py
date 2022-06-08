21d9b8fc479ef54d2128476446bebe114b4a736f
beccaob <rob3651@outlook.com>
2022-06-08 11:09:55 +0000

    add final score and message

--- quiz.py
+++ quiz.py
@@ -12,7 +12,7 @@ while True:
     print("Which mathematical symbol was the title of Ed Sheerans first album in 2011?")
     ans1 = input("A. + \nB. = \nC. % /nAnswer: \n").strip()
     if ans1.upper() == "A":
-        print(Correct!\n)
+        print("Correct!\n")
         score += 1
         break
     elif ans1.upper() == "B" or ans1.upper() == "C":
@@ -20,3 +20,18 @@ while True:
         break
     else: 
         print("Invalid answer! Please choose between A, B or C\n")
+
+    # final score
+    print("You scored" + str(score) + "out of 10!\n")
+
+    # statement for success
+    # or condolances message
+
+    if score <= 4:
+        print("You can do better than that, try again!\n")
+        return score
+    elif score > 4 and score <=7:
+        print("Great job!\n") 
+    else:
+        print("Unreal, well done!")
+        return score
